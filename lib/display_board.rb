d94bb3ecc3df09478dd604e255b2a812593e38cc
DanielMckenzie Mckenzie <drmckenzie89@gmail.com>
2018-02-11 01:31:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,18 +1,5 @@
-board = [" O "," "," "," "," "," "," "," "," "]
-
-def display_board
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
- end
-
-display_board
-
 
 def display_board(board)
-
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
