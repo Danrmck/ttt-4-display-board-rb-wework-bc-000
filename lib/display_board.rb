2ea0ede256edb811fb1bf40f6a954f14e094f84b
DanielMckenzie Mckenzie <drmckenzie89@gmail.com>
2018-02-11 00:48:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -16,11 +16,6 @@ puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]}  "
-end
+ end
 
-
-
-
-
-    #fine display_board that accepts a board and prints
 # out the current state.
